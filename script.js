88a35f1edcb4b83fe68775d89d75bc0b976a14f5
Edison Sim <edison_sxw1@hotmail.com>
2016-08-09 13:47:45 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+// 0. before changing to jquery
+// var btn = document.getElementById('request');
+// var bio = document.getElementById('bio');
+// var url = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/Bio.txt";
+// var request = new XMLHttpRequest();
+
+// btn.addEventListener('click', function() {
+//   this.style.display = 'none';
+//   request.open('GET', url);
+//   request.send();
+// });
+
+// request.onreadystatechange = function() {
+//   if(request.readyState === 4) {
+//     bio.style.border = '1px solid #e8e8e8';
+//     if(request.status === 200) {
+//       bio.innerHTML = request.responseText;
+//     } else {
+//       bio.innerHTML = 'An error occurred during your request: ' +  request.status + ' ' + request.statusText;
+//     }
+//   }
+// }
+
+$(function(){
+
+  // 2. changing to jquery format, notice we don't define the XHR class anymore
+  var $btn = $('#request');
+  var $bio = $('#bio');
+
+  var yoda_url = 'https://yoda.p.mashape.com/yoda?sentence=You+will+learn+how+to+speak+like+me+someday.++Oh+wait.';
+
+
+
+  $btn.on('click', function(e) {
+    e.preventDefault();
+
+    var sentance = $('#yoda').
+
+    $.ajax({
+      // where the data live
+      url: yoda_url,
+      headers: {'X-Mashape-Key': 'PKKkfvpJJlmsh0DCqZgR7QghqloUp1QSMVwjsnp0wTzfE52ZkO'},
+
+      // what is their type
+      dataType: 'html',
+      // show the loader before making the request
+      beforeSend: function() {
+//         $loader.show();
+      }
+    }).done(successFunction)
+      .fail(failFunction);
+    });
+
+    function successFunction(data) {
+    $bio.css('border', '1px solid #e8e8e8');
+    // if data exists
+    if (data.length > 0) {
+      console.log(data);
+
+      $bio.text(data);
+
+    }
+  }
+
+
+  function failFunction(request, textStatus, errorThrown){
+    $bio.text('An error occurred during your request: ' +  request.status + ' ' + textStatus + ' ' + errorThrown);
+  }
+
+
+
+
+
+});
